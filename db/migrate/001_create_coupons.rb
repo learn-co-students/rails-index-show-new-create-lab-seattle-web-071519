23f57525490c3ed5b0b039bd8385366fb499b00b
marshallhelenm <marshallhelenm@gmail.com>
2019-08-11 14:48:04 -0700

    Done.

---
+++ db/migrate/001_create_coupons.rb
@@ -0,0 +1,13 @@
+class CreateCoupons < ActiveRecord::Migration
+    def change
+        create_table :coupons do |t|
+            t.string :coupon_code
+            t.string :store
+
+            t.timestamps null: false
+        end
+    end
+end
+
+
+# You will need to create a coupons table with coupon_code and store columns, which should both be of the string data type.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+coupons = Coupon.create(coupon_code: '1234', store: 'QFC')
